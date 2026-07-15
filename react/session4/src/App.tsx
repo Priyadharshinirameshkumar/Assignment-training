3193738ee30573511c92705f392a5ce1876c44fa
Priyadharshini R <priyadharshinirameshkumar19@gmail.com>
2026-07-15 13:54:32 +0530

    add react/session4-section 2

--- react/session4/src/App.tsx
+++ react/session4/src/App.tsx
@@ -1,9 +1,63 @@
-import PropDrillingDemo from "./components/PropDrillingDemo"
+// import PropDrillingDemo from "./components/PropDrillingDemo"
+
+// function App() {
+//   return (
+//     <div>
+//       <PropDrillingDemo />
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// import Navbar from './components/Navbar'
+// import ThemedCard from './components/ThemedCard'
+
+// function App() {
+//   return (
+//     <div>
+//       <Navbar />
+
+//       <div style={{ padding: '16px' }}>
+//         <ThemedCard name="Rahul" score={92} />
+
+//         <ThemedCard name="Priya" score={78} />
+
+//         <ThemedCard name="Amit" score={45} />
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import Navbar from './components/Navbar'
+import ThemedCard from './components/ThemedCard'
+
+import { useInterns } from './contexts/intern-context'
 
 function App() {
+  const { interns, isLoading } = useInterns()
+
+  if (isLoading) {
+    return <p>Loading interns...</p>
+  }
+
   return (
     <div>
-      <PropDrillingDemo />
+      <Navbar />
+
+      <div style={{ padding: '16px' }}>
+        {interns.map(intern => (
+          <ThemedCard
+            key={intern.id}
+            name={intern.name}
+            score={intern.score}
+          />
+        ))}
+      </div>
     </div>
   )
 }
--- react/session4/src/main.tsx
+++ react/session4/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
+import App from './App'
+
+import { ThemeProvider } from './contexts/theme-context'
+import { InternProvider } from './contexts/intern-context'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeProvider>
+      <InternProvider>
+        <App />
+      </InternProvider>
+    </ThemeProvider>
+  </StrictMode>
 )
